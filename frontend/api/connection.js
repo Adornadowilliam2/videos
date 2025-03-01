63d568e72c0e97a6db5a881e5cc0aaf58bb10790
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-03-01 15:21:09 +0800

    - add this new backend

--- frontend/api/connection.js
+++
@@ -1,3 +0,0 @@
-//export const URL = "http://localhost:8001/api"
-
-export const URL = "http://192.168.56.1:8000/api";
--- frontend/app/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Index from "./home";
-
-export default function index() {
-  return <Index />;
-}
